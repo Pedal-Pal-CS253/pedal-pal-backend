77df2c1090764c7ec91a00a30d2496691fc0c7a2
ANNNA106 <ananyabaghel2004@gmail.com>
2024-02-15 19:51:36 +0530

    models

---
+++ pedalpal/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def getRoutes(requests):
+    routes = [
+        {
+            
+        }
+    ]
+    
+    return JsonResponse()
+    
