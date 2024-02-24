2018bf79d0e910c9edd77274937e4e799bd42da3
Amogh Bhagwat <27415139+AmoghBhagwat@users.noreply.github.com>
2024-02-24 08:19:51 +0000

    remove models

--- pedalpal/booking/models.py
+++ pedalpal/booking/models.py
@@ -1,19 +1,1 @@
 from django.db import models
-
-
-class Ride(models.Model):
-    # define hubs later somewhere else
-
-    start_hub = models.CharField(max_length=20, choices=HUBS)
-    start_time = models.DateTimeField()
-    durarion = models.DurationField()
-
-    end_hub = models.CharField(max_length=20, choices=HUBS)
-    end_time = models.DateTimeField()
-
-
-class Cycle(models.Model):
-    cycle_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    hub = models.CharField(max_length=100, choice=HUBS)
-    locknum = models.IntegerField()
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
