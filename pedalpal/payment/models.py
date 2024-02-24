5ab92523230ddfd2e79aead1696d59111a1a9c5e
AmoghBhagwat <bhagwata22@iitk.ac.in>
2024-02-24 13:15:16 +0530

    add models

---
+++ pedalpal/payment/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+
+class Payment(models.Model):
+    payment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.FloatField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.payment_id)
