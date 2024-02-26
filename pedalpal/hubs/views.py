417c2d0d774479af5f23c7e8a7a49252d638ef02
Kaneez <kaneezfatimamehdi7@gmail.com>
2024-02-26 21:57:09 +0530

    added available cycle field

--- pedalpal/hubs/views.py
+++ pedalpal/hubs/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics,permissions,authentication
 from django.http import JsonResponse
+from django.db.models import Count,Q
 
-from booking.models import Hub
+from rest_framework.response import Response
+
+from booking.models import Hub,Cycle
+from booking.serializers import CycleSerializer
 from .serializers import HubSerializer
 
 class ViewsAPI(generics.GenericAPIView):
@@ -12,7 +16,13 @@ class ViewsAPI(generics.GenericAPIView):
 
     # permission_classes = (permissions.IsAuthenticated,)
     # authentication_classes = (authentication.TokenAuthentication,)
+
+
     def get(self, request, *args, **kwargs):
         queryset = Hub.objects.all()
-        serializer_class = HubSerializer(queryset, many=True)
-        return JsonResponse(serializer_class.data,safe=False)
+        serializer_class = HubSerializer
+        available_cycles = [hub.id for hub in Hub.objects.annotate(num_available=Count('cycle', filter=Q(cycle__booked=False)))]
+        hub_data = serializer_class(queryset, many=True).data
+        for hub in hub_data:
+            hub['available'] = available_cycles[hub_data.index(hub)]
+        return JsonResponse(hub_data,safe=False)
