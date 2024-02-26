553f9c972afa7ecbc7b980c7efe7a2df848d7763
Amogh Bhagwat <27415139+AmoghBhagwat@users.noreply.github.com>
2024-02-26 06:20:23 +0000

    remove serializers.py

--- pedalpal/analytics/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from booking.models import Ride
-
-
-class RideSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ride
-        fields = "__all__"
