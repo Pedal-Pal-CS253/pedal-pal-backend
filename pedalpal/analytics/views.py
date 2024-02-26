ce1c624defdb2c3f4afedc343904e1e55940e2e7
Manglik-R <134364536+Manglik-R@users.noreply.github.com>
2024-02-26 11:32:52 +0530

    analytics added

--- pedalpal/analytics/views.py
+++ pedalpal/analytics/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework import authentication
+from django.http.response import JsonResponse
+from booking.models import Ride
+from booking.serializers import RideSerializer
 
-# Create your views here.
+
+class HistoryViewAPI(generics.ListCreateAPIView):
+    serializer_class = RideSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def get_queryset(self, request):
+        rides = Ride.objects.filter(user=self.request.user)
+        serializer = RideSerializer(rides, many=True)
+        return JsonResponse(serializer.data, safe=False)
