c0f6e16032669d07eab60a2697b658a44ddaad55
Srujan <nsrujan22@iitk.ac.in>
2024-03-07 01:04:47 +0530

    fixed bugs

--- pedalpal/booking/admin.py
+++ pedalpal/booking/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Hub, Cycle, Ride
+from .models import Hub, Cycle, Ride, Lock
 
 admin.site.register(Hub)
 admin.site.register(Cycle)
 admin.site.register(Ride)
+admin.site.register(Lock)
+
