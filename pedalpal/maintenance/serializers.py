986f68d959780e2bb5a878fb087449dd82957be7
Manglik-R <134364536+Manglik-R@users.noreply.github.com>
2024-03-08 22:00:25 +0530

    Updated Feedback

--- pedalpal/maintenance/serializers.py
+++ pedalpal/maintenance/serializers.py
@@ -5,4 +5,11 @@ from maintenance.models import Feedback
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = "__all__"
+        user = Feedback.user
+        fields = [
+            "air_issues",
+            "sound_issues",
+            "brake_issues",
+            "chain_issues",
+            "detailed_issues",
+        ]
