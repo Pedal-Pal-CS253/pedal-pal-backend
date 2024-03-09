17cb375f0216cf09f5f3147936314c8a01c064fe
AmoghBhagwat <bhagwata22@iitk.ac.in>
2024-03-09 13:51:26 +0530

    add payment history

--- pedalpal/payment/views.py
+++ pedalpal/payment/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from django.http.response import JsonResponse
+from .models import Payment
 
 
 class GetBalanceAPI(generics.GenericAPIView):
@@ -17,4 +18,15 @@ class UpdateBalanceAPI(generics.GenericAPIView):
         user = self.request.user
         user.balance += int(request.data.get("amount"))
         user.save()
+
+        if request.data.get("amount") < 0:
+            payment = Payment(
+                user=user, amount=request.data.get("amount"), status="DEBIT"
+            )
+        else:
+            payment = Payment(
+                user=user, amount=request.data.get("amount"), status="CREDIT"
+            )
+        payment.save()
+
         return JsonResponse({"balance": user.balance})
