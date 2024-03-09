b614f051c9312aafb858e0eed73b2f5aaf2d5334
AmoghBhagwat <bhagwata22@iitk.ac.in>
2024-03-09 20:57:11 +0530

    add get transactions API

--- pedalpal/payment/urls.py
+++ pedalpal/payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from payment.views import GetBalanceAPI, UpdateBalanceAPI
+from payment.views import GetBalanceAPI, UpdateBalanceAPI, GetTransactionsAPI
 
 urlpatterns = [
     path("get_balance/", GetBalanceAPI.as_view(), name="get_balance"),
     path("update_balance/", UpdateBalanceAPI.as_view(), name="update_balance"),
+    path("get_transactions/", GetTransactionsAPI.as_view(), name="get_transactions"),
 ]
