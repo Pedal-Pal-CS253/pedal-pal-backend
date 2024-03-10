9def17079a41928a100354f1fec2f228bd58db25
Manglik-R <134364536+Manglik-R@users.noreply.github.com>
2024-03-10 11:41:45 +0530

    User feedbacks

--- pedalpal/maintenance/serializers.py
+++ pedalpal/maintenance/serializers.py
@@ -5,11 +5,4 @@ from maintenance.models import Feedback
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = [
-            "user",
-            "air_issues",
-            "sound_issues",
-            "brake_issues",
-            "chain_issues",
-            "detailed_issues",
-        ]
+        fields = "__all__"
--- pedalpal/maintenance/urls.py
+++ pedalpal/maintenance/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from maintenance import views
 
-urlpatterns = [path("feedbacks/", views.FeedbackList.as_view(), name="feedback_list")]
+urlpatterns = [
+    path("feedbacks/", views.FeedbackList.as_view(), name="feedback_list"),
+    path("feedbacks/add/", views.FeedbackAdd.as_view(), name="feedback_add"),
+]
