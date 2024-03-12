9a3993c52efd29c181968ec8c2d227bb30b1d73f
AmoghBhagwat <bhagwata22@iitk.ac.in>
2024-03-12 14:31:57 +0530

    fix view hubs api

--- pedalpal/booking/urls.py
+++ pedalpal/booking/urls.py
@@ -1,10 +1,10 @@
-from .views import BookNowAPI, EndRideAPI, BookLaterAPI, ViewsAPI
+from .views import BookNowAPI, EndRideAPI, BookLaterAPI, GetHubsDataAPI
 from django.urls import path
 
 
 urlpatterns = [
     path("book/", BookNowAPI.as_view(), name="book"),
-    path("view_hubs/", ViewsAPI.as_view()),
+    path("view_hubs/", GetHubsDataAPI.as_view()),
     path("end/", EndRideAPI.as_view(), name="end"),
     path("book_later/", BookLaterAPI.as_view(), name="book_later"),
 ]
