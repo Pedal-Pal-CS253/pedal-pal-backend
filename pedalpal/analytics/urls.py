7ae2e6ad66e550a17125dd23a65398fb0c7950bd
Amogh Bhagwat <27415139+AmoghBhagwat@users.noreply.github.com>
2024-03-14 23:17:47 +0530

    Update urls.py

--- pedalpal/analytics/urls.py
+++ pedalpal/analytics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from analytics.views import HistoryViewAPI, UserUpdateAPI
+from analytics.views import HistoryViewAPI, UserUpdateAPI, BookingViewAPI
 
 urlpatterns = [
     path("history/", HistoryViewAPI.as_view(), name="Ride History"),
     path("settings/", UserUpdateAPI.as_view(), name="User Settings and Profile Update"),
+    path("booking_history/", BookingViewAPI.as_view(), name="Booking History"),
 ]
