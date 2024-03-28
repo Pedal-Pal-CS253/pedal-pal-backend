47e0f6653d4e18a9e543fcb7a21f1a9a14d1b13e
ANNNA106 <ananyabaghel2004@gmail.com>
2024-03-28 15:46:26 +0530

    payment and maintenance tests

--- pedalpal/payment/tests.py
+++ pedalpal/payment/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Payment
+from authentication.models import Profile
 
-# Create your tests here.
+
+class PaymentTestCase(APITestCase):
+    def test_add_payment(self):
+        self.client.force_authenticate(user=self.user)
+        data = {
+            "amount":2000, 
+        }
+        response = self.client.post("/payment/add_payment/", data)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_balance(self):
+        self.client.force_authenticate(user=self.user)
+        data = {
+            "amount":1000, 
+        }
+        response = self.client.post("/payment/update_balance/", data)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_balance(self):
+        self.client.force_authenticate(user=self.user)
+        data = {}
+        response = self.client.get("/payment/get_balance/", data)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_transaction(self):
+        self.client.force_authenticate(user=self.user)
+        data = {}
+        response = self.client.get("/payment/get_transactions/", data)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)      
+        
+    def setUp(self):
+        self.user = Profile.objects.create(
+            email="test@user.com",
+            first_name="Test",
+            last_name="User",
+            phone="1234567890",
+            password="test1234",
+        )
+        
